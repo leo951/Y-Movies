88036539344e87bb684e83aa007287c2d7140573
Aaron Saksik <aaron.saksik@ynov.com>
2021-01-04 10:39:22 +0100

    image

--- app/src/main/java/com/example/y_movies/ui/poster/PosterMovieActivity.java
+++ app/src/main/java/com/example/y_movies/ui/poster/PosterMovieActivity.java
@@ -1,6 +1,7 @@
 package com.example.y_movies.ui.poster;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -171,4 +172,7 @@ public class PosterMovieActivity extends AppActivity {
         }
     }
 
+    public void addPreference(View view) {
+
+    }
 }
